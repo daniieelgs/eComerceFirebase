2053554e42b4883d3292543bb29ea93e81f5a93d
Daniel Garcia <daniieelgs@gmail.com>
2022-04-01 00:01:22 +0200

    eComerce con Firebase

---
+++ js/config.js
@@ -0,0 +1,11 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyB3rifYS6w9Jl9SaKUfUJFYwGarilN2WzA",
+    authDomain: "desenvolupament-web.firebaseapp.com",
+    databaseURL: "https://desenvolupament-web-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "desenvolupament-web",
+    storageBucket: "desenvolupament-web.appspot.com",
+    messagingSenderId: "546317940028",
+    appId: "1:546317940028:web:a0f5f067a95db80b451a39"
+});
+
+const auth = firebase.auth();
